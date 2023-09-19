d7279aa5467703e6ad03463cee3fe2e504eb74be
leonardo oliveira <azideiamano20209@gmail.com>
2023-09-19 09:52:02 -0300

    renomeação e ajuste

--- formulario.js
+++ formulario.js
@@ -1,24 +1,15 @@
 function validar(){
-    let nome = document.getElementById('nome1').value;
     let senha = document.getElementById('senhax').value;
     let email = document.getElementById('email1').value;
-    let data = document.getElementById('nascimento1').value;
-    let telefone = document.getElementById('telefone').value;
-    let cpf = document.getElementById('cpf1').value;
-    let cep = document.getElementById('cep1').value;
-    let rua = document.getElementById('rua1').value;
-    let bairro = document.getElementById('bairro1').value;
-    let estado = document.getElementById('estado1').value;
-    let cidade = document.getElementById('cidade1').value;
-    let numero = document.getElementById('numero1').value;
     let senhaconf = document.getElementById('senhaConf').value;
 
 
 
 
-    if(!email || !senha || !nome || !data || !telefone || !cpf || !cep || !cpf || !rua || !bairro || !estado || !cidade || !numero || !senhaconf){
+    if(!email || !senha || !senhaconf){
     alert("você deixou campos vazios,por favor preecher!")
 }else{
         alert("obrigado por prenecher todas as informaçoes");
     }
+    
 }
